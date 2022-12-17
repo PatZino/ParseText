1e2c7fb5dfccd7374605304e6eb27dd07f69bbc3
Patricia Edaware <eedawarez@gmail.com>
2022-12-17 16:13:22 +0000

    Created a method to display and count each line in a text file

---
+++ DisplayAndCountLines.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseText
+{
+	public class DisplayAndCountLines
+	{
+        public static void Execute() {
+            int counter = 0;
+            var filePath = @"c:\Zeep\ICD-10Code.txt";
+
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(filePath))
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            Console.ReadLine();
+        }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,23 +6,7 @@ namespace ParseText
     {
         static void Main(string[] args)
         {
-            int counter = 0;
-
-            // Read the file and display it line by line.  
-            foreach (string line in System.IO.File.ReadLines(@"c:\Zeep\ICD-10Code.txt"))
-            {
-                System.Console.WriteLine(line);
-                counter++;
-            }
-
-            var query = String.Format(@"hello all");
-            System.Console.WriteLine("There were {0} lines.", counter);
-            // Suspend the screen.  
-            System.Console.ReadLine();
-
             ExtractCSV.ExtractCsvData();
-            Console.WriteLine("Hello World!");
-
         }
     }
 }
